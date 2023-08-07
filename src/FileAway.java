84e46dbedb47a5f1b68333bf39f61b556e32fd7c
Kaurvi <135301950+Kaurvi@users.noreply.github.com>
2023-08-07 01:06:05 -0400

    FileAway.java

--- src/FileAway.java
+++ src/FileAway.java
@@ -6,58 +6,60 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import static java.nio.file.StandardOpenOption.CREATE;
-public class FileAway
-{
-    public static void main(String[] args)
-    {
-        JFileChooser =new JFileChooser ();
+public class FileAway {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser ();
         File selectedFile;
-        String
-        ArrayList <String>recs = new ArrayList<>();
-        recs.add("Sample data for our file writing example.");
-        recs.add("Sample data Line 2.");
-        recs.add("Sample data Line 3.");
-        recs.add("Sample data Line 4.");
-        recs.add("Sample data Line 5.");
+        String line = "";
+        ArrayList<String> lines = new ArrayList<> ();
+        try {
+
+            File workingDirectory = new File ( System.getProperty ( "user.dir" ) );
+            chooser.setCurrentDirectory ( workingDirectory );
+
+            if (chooser.showOpenDialog ( null ) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile ();
+                Path file = selectedFile.toPath ();
+                InputStream in =
+                        new BufferedInputStream ( Files.newInputStream ( file, CREATE ) );
+                BufferedReader reader =
+                        new BufferedReader ( new InputStreamReader ( in ) );
+
+                int lineCount = 0;
+                int wordCount = 0;
+                int characterNum = 0;
+
+                while (reader.ready ()) {
+                    line = reader.readLine ();
+                    lines.add ( line );
+                    lineCount++;
+
+                    System.out.printf ( "\nLine %4d %-60s ", lineCount, line );
+                    String[] words = line.split ( " " );
+                    wordCount = wordCount + words.length;
+
+                    characterNum = characterNum + line.length ();
+                    String name = selectedFile.getName ();
+
+                }
+                reader.close ();
+                System.out.println ( "\n\nData file read!" );
+                System.out.println ( "\n\n" );
+                String name = selectedFile.getName ();
+                System.out.println ( "Name of File" + name );
+                System.out.println ( "Number OF Lines in the File" + lineCount );
+                System.out.println ( "Number OF words in the File" + wordCount );
+                System.out.println ( "Number OF character in the File" + characterNum );
 
 
-        // uses a fixed known path:
-        //  Path file = Paths.get("c:\\My Documents\\data.txt");
-
-        // use the toolkit to get the current working directory of the IDE
-        // will create the file within the Netbeans project src folder
-        // (may need to adjust for other IDE)
-        // Not sure if the toolkit is thread safe...
-        File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
-
-        try
-        {
-            // Typical java pattern of inherited classes
-            // we wrap a BufferedWriter around a lower level BufferedOutputStream
-            OutputStream out =
-                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(out));
-
-            // Finally can write the file LOL!
-
-            for(String rec : recs)
-            {
-                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
-                writer.newLine();  // adds the new line
-
+            } else {
+                System.out.println ( "ERROR!!! ... exiting.\nRun the program again and select a file." );
             }
-            writer.close(); // must close the file to seal it and flush buffer
-            System.out.println("Data file written!");
+        } catch (FileNotFoundException e) {
+            System.out.println ( "File not found!!!" );
+            e.printStackTrace ();
+        } catch (IOException e) {
+            e.printStackTrace ();
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
     }
-
 }
